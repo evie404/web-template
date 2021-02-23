fafa8bebd2b5c897492a4e3d933ec670423868bc
Ricky Pai <rickyp999+github@gmail.com>
2021-02-22 22:10:29 -0800

    move phone-api to own dir

--- api/main.go
+++ api/main.go
@@ -6,8 +6,8 @@ import (
 	"net/http"
 
 	"github.com/rickypai/web-template/api/extauth"
+	"github.com/rickypai/web-template/api/phone-api/server"
 	"github.com/rickypai/web-template/api/protobuf/phone"
-	"github.com/rickypai/web-template/api/server"
 	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 )
