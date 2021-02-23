d273e3e2580a40cf76eca392af6826cdd7c9b4aa
Ricky Pai <rickyp999+github@gmail.com>
2021-02-22 22:16:15 -0800

    rpc.

--- api/phone-api/server/server.go
+++ api/phone-api/server/server.go
@@ -7,25 +7,25 @@ import (
 	"github.com/golang/protobuf/ptypes"
 	makePb "github.com/rickypai/web-template/api/protobuf/make"
 	"github.com/rickypai/web-template/api/protobuf/os"
-	"github.com/rickypai/web-template/api/protobuf/phone"
+	rpc "github.com/rickypai/web-template/api/protobuf/phone"
 	"github.com/rickypai/web-template/api/server/cursor"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 type Server struct {
-	phone.UnimplementedPhoneServiceServer
+	rpc.UnimplementedPhoneServiceServer
 }
 
-func (s *Server) ListByPage(ctx context.Context, req *phone.ListByPageRequest) (*phone.ListByPageResponse, error) {
+func (s *Server) ListByPage(ctx context.Context, req *rpc.ListByPageRequest) (*rpc.ListByPageResponse, error) {
 	page, cursor, count := cursor.GetPageOptions(req)
-	results := make([]*phone.Phone, 0, count)
+	results := make([]*rpc.Phone, 0, count)
 
 	for i := cursor + 1; i < cursor+1+int64(count); i++ {
 		results = append(results, getPhone(i))
 	}
 
-	return &phone.ListByPageResponse{
+	return &rpc.ListByPageResponse{
 		Results:    results,
 		NextPage:   page + 1,
 		HasNext:    true,
@@ -33,9 +33,9 @@ func (s *Server) ListByPage(ctx context.Context, req *phone.ListByPageRequest) (
 	}, nil
 }
 
-func (s *Server) ListByCursor(ctx context.Context, in *phone.ListByCursorRequest) (*phone.ListByCursorResponse, error) {
+func (s *Server) ListByCursor(ctx context.Context, in *rpc.ListByCursorRequest) (*rpc.ListByCursorResponse, error) {
 	cursor, count := cursor.GetCursorOptions(in)
-	results := make([]*phone.Phone, 0, count)
+	results := make([]*rpc.Phone, 0, count)
 
 	for i := cursor + 1; i < cursor+1+int64(count); i++ {
 		results = append(results, getPhone(i))
@@ -43,13 +43,13 @@ func (s *Server) ListByCursor(ctx context.Context, in *phone.ListByCursorRequest
 
 	nextPageCursor := results[len(results)-1].GetId()
 
-	return &phone.ListByCursorResponse{
+	return &rpc.ListByCursorResponse{
 		Results: results,
 		Cursor:  nextPageCursor,
 	}, nil
 }
 
-func (s *Server) GetOneByID(ctx context.Context, in *phone.GetOneByIDRequest) (*phone.GetOneByIDResponse, error) {
+func (s *Server) GetOneByID(ctx context.Context, in *rpc.GetOneByIDRequest) (*rpc.GetOneByIDResponse, error) {
 	fmt.Printf("Called GetPhone with %+v\n", in)
 
 	id := in.GetId()
@@ -62,15 +62,15 @@ func (s *Server) GetOneByID(ctx context.Context, in *phone.GetOneByIDRequest) (*
 		return nil, status.Error(codes.Unknown, "unknown")
 	}
 
-	return &phone.GetOneByIDResponse{
+	return &rpc.GetOneByIDResponse{
 		Result: getPhone(id),
 	}, nil
 }
 
-func getPhone(id int64) *phone.Phone {
+func getPhone(id int64) *rpc.Phone {
 	ts := ptypes.TimestampNow()
 
-	return &phone.Phone{
+	return &rpc.Phone{
 		Id:   id,
 		Name: fmt.Sprintf("Phone #%v", id),
 		Make: &makePb.Make{
