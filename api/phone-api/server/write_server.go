4a11ac05eca98c31733bd61c068bd15db94d819b
Ricky Pai <rickyp999+github@gmail.com>
2021-02-23 18:49:43 -0800

    writer servers

---
+++ api/phone-api/server/write_server.go
@@ -0,0 +1,37 @@
+package server
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/rickypai/web-template/api/phone-api/repo"
+	rpc "github.com/rickypai/web-template/api/protobuf/phone"
+)
+
+type modelTWriter interface {
+	CreateOne(ctx context.Context, rpcInstance modelT) (modelT, error)
+}
+
+func NewWriteServer(db *sql.DB) *WriteServer {
+	return &WriteServer{
+		repo: repo.NewWriter(db),
+	}
+}
+
+type WriteServer struct {
+	rpcT
+
+	repo modelTWriter
+}
+
+func (s *WriteServer) CreateOne(ctx context.Context, req *rpc.CreateOneRequest) (*rpc.CreateOneResponse, error) {
+	result, err := s.repo.CreateOne(ctx, req.GetRequest())
+	if err != nil {
+		return nil, fmt.Errorf("error creating %s: %w", modelName, err)
+	}
+
+	return &rpc.CreateOneResponse{
+		Result: result,
+	}, nil
+}
