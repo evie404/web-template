31f8d8f0fbcd165998bf4fd70069968d58ac32bf
Ricky Pai <rickyp999+github@gmail.com>
2021-03-05 14:23:57 -0800

    home that uses same layout

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,32 +1,12 @@
-import { Layout, Menu, Breadcrumb } from "antd";
-
-const { Header, Content, Footer } = Layout;
+import React from "react";
+import Container from "../components/container";
+import SEO from "../components/seo";
 
 const Home: React.FC = () => (
-  <Layout className="layout">
-    <Header>
-      <div className="logo" />
-      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
-      </Menu>
-    </Header>
-    <Content style={{ padding: "0 50px" }}>
-      <Breadcrumb style={{ margin: "16px 0" }}>
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
-      <div className="site-layout-content">
-        <h1>Content</h1>
-        <p>hi</p>
-      </div>
-    </Content>
-    <Footer style={{ textAlign: "center" }}>
-      Ant Design ©2018 Created by Ant UED
-    </Footer>
-  </Layout>
+  <Container defKey="0">
+    <SEO title="Home" />
+    <div>Home</div>
+  </Container>
 );
 
 export default Home;
