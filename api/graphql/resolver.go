b9f76d4bc89fa2ca3e207ec1433d70d1ca9164d3
Ricky Pai <rickyp999+github@gmail.com>
2021-03-19 11:15:29 -0700

    configure resolver

--- api/graphql/resolver.go
+++ api/graphql/resolver.go
@@ -4,4 +4,18 @@ package graphql
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+import (
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/rickypai/web-template/api/ent"
+	"github.com/rickypai/web-template/api/graphql/generated"
+)
+
+// Resolver is the resolver root.
+type Resolver struct{ client *ent.Client }
+
+// NewSchema creates a graphql executable schema.
+func NewSchema(client *ent.Client) graphql.ExecutableSchema {
+	return generated.NewExecutableSchema(generated.Config{
+		Resolvers: &Resolver{client},
+	})
+}
